28c5becc80177ea3f3f4c70f82457a995c192246
Jasmine <jasmineparks991@gmail.com>
2026-04-09 23:16:45 -0500

    Create function that takes a string and returns it reversed

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(strg) {
+    strg.split('').reverse().join();
 };
-
+alert( strg )
 // Do not edit below this line
 module.exports = reverseString;
