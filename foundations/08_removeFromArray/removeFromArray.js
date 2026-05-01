1fbace7a3ac122710c35d8057d18502d29712e87
Jasmine <jasmineparks991@gmail.com>
2026-04-30 22:43:20 -0500

    Save JS exercise progress

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+let arr = [1, 2, 3, 4];
+
+const removeFromArray = function(a, b ...itemsToRemove) {
+    if (i === a || i === b)
+    arr.filter(itemsToRemove)
 };
+
+
+removeFromArray([1, 2, 3, 4], 3);
+// return should be 1, 2, 4
 
 // Do not edit below this line
 module.exports = removeFromArray;
